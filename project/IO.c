06efc131daafb48e82ee37ef2c496403ffb01aae
ridham <ridhambhagat@gmail.com>
2020-03-27 20:51:48 +0530

    csv reading files

---
+++ project/IO.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+struct data{
+	int obs;
+	char a[100];
+	char b[100];
+	char c[100];
+	int d;
+	int e;
+};
+struct data def;
+void removeSpaces(char line[]){
+	
+int i=0;
+int l=strlen(line);
+int j=0;
+
+	while(line[j]!='\0'){
+		 if(line[j]==' '){
+		 	l-=1;
+		 }
+		
+		 ++j;
+	}
+	while(line[strlen(line)-1]==' '){
+		line[strlen(line)-1]='\0';
+	}
+	while(line[i]!='\0'){
+		if(line[i]==' '){
+			int j=i;
+			int k=i;
+			while(line[j]==' '){
+				j++;
+				
+			}
+		
+						while(j<strlen(line)){
+				line[k]=line[j];
+				k++;j++;
+			}
+			continue;
+		}
+		i++;
+	}
+	line[l]='\0';
+
+}
+void removeCommas(char line[]){
+	removeSpaces(line);
+	int i=0;
+	while(line[i]!='\0'){
+		if(line[i]==','){
+			line[i]=' ';
+		}
+		i++;
+	}
+}		
+int main(int argc, char const *argv[])
+{
+	FILE *fp; //a file pointer 
+	fp=fopen("test.csv","r"); //opening file in read mode
+	char x[100];
+	printf("%s\n",a);
+		while(fgets(x,100,fp)!=NULL){
+			
+		removeCommas(x);
+		printf("\n");
+		sscanf(x,"%d %s %s %s %d %d",&(def.obs),def.a,def.b,def.c,&(def.d),&(def.e));
+		printf("%d\n",def.obs);
+		printf("%s\n",def.a);
+		printf("%s\n", def.b);
+		printf("%s\n", def.c);
+		printf("%d\n",def.d);
+		printf("%d\n",def.e);
+		
+		// 
+}
+	
+	fclose(fp);
+
+	return 0;
+}
---
+++ project/Project.h
@@ -0,0 +1,55 @@
+// Basically different structures so that we can access the databases namely customer database and employee database
+typedef struct employee
+{
+    int employee_id;
+    char emplyee_name[20];
+    int  phno;
+    char shift{10];
+}employee;
+
+typedef struct customer
+{
+    int customer_id;
+    char cust_name[20];
+    char item[25];
+    float item_price;
+    int code; //so this code represents whether the item is fragile or not 0 is fragile and 1 for not fragile if you want we can change it to char
+    int phno;
+    char email_ID[30];
+    char address[20];
+}customer;
+
+typedef struct delivery
+{
+    employee emp;//so this is to have all the employee fields inside
+    customer cust;//""""" customer"""
+    int priority;
+    struct delivery next;
+}del;
+
+//Basically we could have done with just a single structure but if we are linking a database we need multiple structures to access the databases
+
+customer getcust(int); //to access the database customer and return a particular customer using customer_id
+
+employee getemployee(customer); //so this function will return the employee assigned taking the customer address basically we'll make the list for each and every employee of the organization and the employee assigned will depend on the customer
+
+//Queue functions so these functions for a particular employee will make the list of customers for that day we get one prob here that database has to be updated daily
+void createpqueue();
+void push();//insertion in the queue
+delivery *pop();
+void assign_priority();// Yet to be decided decide asap
+void display_list(employee);
+// So these functions will be in an loop which will be traversing through the employee database and we will have to make different queues for different employees
+
+//These are functions for the databases created mns inserting data and deleting data in them they will be an option in case ma'am wants to enter some details into our dtabses
+void insert_employee();
+void insert_customer();
+void delete_employee();
+void delete_customer();
+
+// Small functions in case we need it
+void swap_data();
+int Isempty();
+
+// So basically we have 10 major functions in the program so we have to work accordingly these function we'll divide and then compile inside main
+// Also think about how our main function should look All the printf should be used carefully because the output should look good
