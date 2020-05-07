92787a6060e365b0904acb5c0a11a14d8aa7b930
Arinjay-hub <62749988+Arinjay-hub@users.noreply.github.com>
2020-05-07 23:48:15 +0530

    Update project.c

--- project/project.c
+++ project/project.c
@@ -1,8 +1,18 @@
 void assign_priority()
 {
     int priority=0;
-    if(price>10000)
-        priority++;
-    if(code==1)
-        priority=priority+2;
+    if(prime==1)
+    {
+        if(price>8000)
+            priority++;
+        if(code==1)
+            priority=priority+2;
+    }
+    else
+    {
+        if(price>12000)
+            priority++;
+        if(code==1)
+            priority=priority+2;
+    }   
 }
