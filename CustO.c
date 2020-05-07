ec039b171544f76cdafc7fb7812b9b05db320b4c
Prabhat Kumar Singh Gaur <64524919+pksg2001@users.noreply.github.com>
2020-05-08 00:42:15 +0530

    Update CustO.c

--- CustO.c
+++ CustO.c
@@ -1,14 +1,16 @@
-printf("\n\nEnter the Customer ID: %d",c1.customer_id);
+void print(customer* c)
+{
+    printf("\n\nEnter the Customer ID: %d",c->customer_id);
 printf("The Customer Name: ");
-puts(c1.cust_name);
+puts(c->cust_name);
 printf("\nThe item name: ");
-puts(c1.item);
-printf("\nThe item price: %f",c1.item_price);
-printf(" item is fragile or not(0 for not 1 for yes): %d",c1.code);
+puts(c->item);
+printf("\nThe item price: %f",c->item_price);
+printf(" item is fragile or not(0 for not 1 for yes): %d",c->code);
 printf("The Email ID of the customer: ");
-puts(c1.email_ID);
-printf(""The address of the customer: ");
-puts(c1.address);
-printf("The pincode of the customer: %d",c1.pincode);
-printf("The customer has prime membership or not(1 if he has and 0 if he doesn't): %d",c1.prime);
-
+puts(c->email_ID);
+printf("The address of the customer: ");
+puts(c->address);
+printf("The pincode of the customer: %d",c->pincode);
+printf("The customer has prime membership or not(1 if he has and 0 if he doesn't): %d",c->prime);
+}
