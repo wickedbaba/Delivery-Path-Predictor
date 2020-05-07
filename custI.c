6ee5b3aaa3f61b0149a337c0bc9f62407b446ef9
Prabhat Kumar Singh Gaur <64524919+pksg2001@users.noreply.github.com>
2020-05-08 00:44:39 +0530

    Update custI.c

--- custI.c
+++ custI.c
@@ -1,22 +1,24 @@
 
+void input(customer* c)
+{
 printf("\n\nEnter the Customer ID: ");
-scanf("%d",&c1.customer_id);
+scanf("%d",&c->customer_id);
 printf("Enter Customer Name: ");
-scanf("%s",c1.cust_name);
+scanf("%s",c->cust_name);
 printf("Enter the item name: ");
-scanf("%s",c1.item);
+scanf("%s",c->item);
 printf("Enter the item price: ");
-scanf("%f",&c1.item_price);
+scanf("%f",&c->item_price);
 printf("Enter whether item is fragile or not(0 for not 1 for yes): ");
-scanf("%d",&c1.code);
+scanf("%d",&c->code);
 printf("Enter the Email ID of the customer: ");
-scanf("%s",c1.email_ID);
+scanf("%s",c->email_ID);
 printf("Enter the address of the customer: ");
-scanf("%s",&c1.address);
+scanf("%s",&c->address);
 printf("Enter the pincode of the customer: ");
-scanf("%d",&c1.pincode);
+scanf("%d",&c->pincode);
 printf("Enter the whether the customer has prime membership or not(1 if he has and 0 if he doesn't):")
-scanf("%d",&c1.prime);
-
+scanf("%d",&c->prime);
+}
 
 
