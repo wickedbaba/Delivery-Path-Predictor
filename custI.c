51ce6e64207b9fd71363886ee5f63deb21e7ce9d
Arinjay-hub <62749988+Arinjay-hub@users.noreply.github.com>
2020-05-07 22:52:40 +0530

    Add files via upload

---
+++ custI.c
@@ -0,0 +1,22 @@
+
+printf("\n\nEnter the Customer ID: ");
+scanf("%d",&c1.cust_name);
+printf("Enter Customer Name: ");
+scanf("%s",c1.cust_name);
+printf("Enter the item name: ");
+scanf("%s",c1.item);
+printf("Enter the item price: ");
+scanf("%f",&c1.item_price);
+printf("Enter whether item is fragile or not(0 for not 1 for yes): ");
+scanf("%d",&c1.code);
+printf("Enter the Email ID of the customer: ");
+scanf("%s",c1.email_ID);
+printf("Enter the address of the customer: ");
+scanf("%s",&c1.address);
+printf("Enter the pincode of the customer: ");
+scanf("%d",&c1.pincode);
+printf("Enter the whether the customer has prime membership or not(1 if he has and 0 if he doesn't):")
+scanf("%d",&c1.prime);
+
+
+
